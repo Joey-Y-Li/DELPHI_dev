1b459b359ff8f44d8269def5430c09bfbddbaaa3
Yiwei Li <yiwei.li1989@gmail.com>
2020-08-14 17:07:57 -0400

    Update genMSA.sh
    updated the database error for ECO computation

--- feature_computation/ECO/genMSA.sh
+++ feature_computation/ECO/genMSA.sh
@@ -15,7 +15,7 @@ do
 	echo  ${outpath}/"$(basename "$file")"
 	if [ ! -f ${outpath}/"$(basename "$file")" ]; then
 		echo "ECO file for $file doesn't exist, running hhblits"
-		${hhblits} -i $file -ohhm ${outpath}/"$(basename "$file")"  -d ${blast_nr_db}  -hide_cons -hide_pred -hide_dssp -v 0  -neffmax 1 -n 1 -cpu 10
+		${hhblits} -i $file -ohhm ${outpath}/"$(basename "$file")"  -d ${database}  -hide_cons -hide_pred -hide_dssp -v 0  -neffmax 1 -n 1 -cpu 10
 	else
 		echo "ECO file for $file exists, skip hhblits"
 	fi
